148614daf6c9c973034a654a1d7c14519922ec0d
Clemens <clemensrubenschumann@googlemail.com>
2025-06-04 02:21:36 +0800

    Advanced writing of zx2stim

--- quizx/src/zx2stim.rs
+++ quizx/src/zx2stim.rs
@@ -1,6 +1,10 @@
+/// Although the extract module already exists, it only works on graphs
+/// that have gflow, which particularly QEC graphs don't, due to their
+/// mid-circuit measurements
+/// Hence, this new zx2stim module for qec noise simulations/thresholds
 use crate::hash_graph::Graph;
 use crate::graph::GraphLike;
-use crate::graph::VType::{X, Z, H};
+use crate::graph::VType::{X, Z, H, B};
 use ordered_float::OrderedFloat;
 use std::collections::BTreeMap;
 
@@ -17,9 +21,39 @@ pub fn zx_to_stim(graph: &Graph) -> String {
     // Loop over rows in order
     for (_row, nodes) in nodes_by_row {
         for node in nodes {
-            // This is where you would match on node type and write Stim gates
-            stim_lines.push(format!("# node {}", node));
-            stim_lines.push(format!("Type: {:?}", graph.vertex_type(node)));
+            // This is all assuming its a bipartite phaseless diagram
+            // stim_lines.push(format!("# node {}", node));
+            // stim_lines.push(format!("Type: {:?}", graph.vertex_type(node)));
+            let vtype = graph.vertex_type(node);
+            let neighbors: Vec<_> = graph.neighbors(node).collect();
+            let qubit = graph.qubit(node);
+            let row = graph.row(node);
+            log::debug!("Currently on node {}, qubit {}, row {}, and have neighbors {:?}", node, qubit, row, neighbors);
+            match neighbors.len() {
+                1 => {
+                    let neighbor = neighbors[0];
+                    let neighbor_row = graph.row(neighbor);
+                    log::debug!("node row: {}\n neighbor row: {}",row, neighbor_row);
+                    if vtype == B {
+                        continue;
+                    }
+                    if vtype == Z {
+                            stim_lines.push(format!("H {}", qubit));
+                        }
+                    if neighbor_row < row {
+                        stim_lines.push(format!("MR {}", qubit))
+                    }
+                }
+                2 => {}
+                3 => {}
+                _ => {
+                    stim_lines
+                    .push
+                    (format!("# Unsupported node degree {}: {}", 
+                    neighbors.len(), node))
+                }
+            }
+            
         }
         stim_lines.push("TICK".to_string());
     }
@@ -30,26 +64,54 @@ pub fn zx_to_stim(graph: &Graph) -> String {
 mod tests {
     use super::*;
     use crate::graph_loader::load_graph;
+    use crate::graph_to_svg::graph_to_svg;
+    use std::sync::Once;
 
-    #[test]
-    fn test_zx2stim()-> () {
-        let mut graph = Graph::new();
-        let v1 = graph.add_vertex(X);
-        let v2 = graph.add_vertex(Z);
-        graph.add_edge(v1, v2);
+    static INIT: Once = Once::new();
 
-        print!("{}",zx_to_stim(&graph));
+    fn init_logger() {
+        INIT.call_once(|| {
+            env_logger::builder()
+                .is_test(true)
+                .filter_level(log::LevelFilter::Debug)
+                .try_init()
+                .ok();
+        });
     }
+    fn test_file(name: &str) -> String {
+        std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("../")
+            .join("test_files")
+            .join(name)
+            .to_str()
+            .unwrap()
+            .to_string()
+    }
+
+    // #[test]
+    // fn test_zx2stim()-> () {
+    //     init_logger();
+    //     let mut graph = Graph::new();
+    //     let v1 = graph.add_vertex(X);
+    //     let v2 = graph.add_vertex(Z);
+    //     graph.add_edge(v1, v2);
+
+    //     println!("{}",zx_to_stim(&graph));
+    // }
 
     
     #[test]
     fn better_test_zx2stim() {
+        init_logger();
         let path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .unwrap()
         .join("test_files")
-        .join("xx_stab.zxg");
+        .join("simple_meas.zxg");
         let graph = load_graph(path.to_str().unwrap());
+        let svg = graph_to_svg(&graph, &[]);
+        let outpath = "test_write.svg";
+        let _ = std::fs::write(test_file(outpath), svg);
         println!("{}", zx_to_stim(&graph));
     }
 }
